e2e2a68f69251a554326bdcca5e3144d702dc366
Diego Vazquez <diegovazquez@Diegos-MBP.lan>
2021-02-21 16:57:01 -0500

    fix merge conflicts

--- components/home/navBar.js
+++ components/home/navBar.js
@@ -10,22 +10,14 @@ export default function MobileNav() {
   const [showNav, setShowNav] = useState(false);
 
   const hideList = (e) => {
-<<<<<<< HEAD
-    listRef.current.style.top = "-50vh";
-=======
     listRef.current.style.left = '-50vh';
->>>>>>> main
     setShowNav(false);
   };
 
   const handleNav = (e) => {
     if (!showNav) {
-<<<<<<< HEAD
-      listRef.current.style.top = "60px";
-=======
       listRef.current.style.left = '0px';
       listRef.current.style.top = '60px';
->>>>>>> main
       setShowNav(true);
     } else {
       hideList();
@@ -34,17 +26,12 @@ export default function MobileNav() {
 
   return (
     <nav className={styles.root}>
-<<<<<<< HEAD
-      <div className={styles.logo}>Brian's Logo</div>
-      <input type="checkbox" id="click" className={styles.click} />
-=======
       <div className={styles.logo}>
         <a href="/">
           Brian's Logo
         </a>
       </div>
       <input type="checkbox" id="click" className={styles.click}/>
->>>>>>> main
       <label htmlFor="click" className={styles.menuBtn}>
         {showNav ? (
           <CloseIcon onClick={handleNav} className={styles.bars} />
@@ -52,29 +39,22 @@ export default function MobileNav() {
           <DehazeIcon onClick={handleNav} className={styles.bars} />
         )}
       </label>
-<<<<<<< HEAD
       <ul className={styles.list} ref={listRef}>
+        <li className={styles.listItem}>
+          <a href="/search" className={styles.listItemLink} onClick={hideList}>
+            SEARCH
+          </a>
         <li className={styles.listItem}>
           <a href="/about" className={styles.listItemLink} onClick={hideList}>
             ABOUT ME
           </a>
         </li>
-        <li className={styles.listItem}>
-          <a href="#" className={styles.listItemLink} onClick={hideList}>
-            SOMETHING ELSE
-          </a>
         </li>
         <li className={styles.listItem}>
           <a href="/contact" className={styles.listItemLink} onClick={hideList}>
             CONTACT ME
           </a>
         </li>
-=======
-      <ul className={styles.list} ref={listRef}> 
-        <li className={styles.listItem}><a href="/search" className={styles.listItemLink} onClick={hideList}>SEARCH</a></li>
-        <li className={styles.listItem}><a href="/about" className={styles.listItemLink} onClick={hideList}>ABOUT ME</a></li>
-        <li className={styles.listItem}><a href="/contact" className={styles.listItemLink} onClick={hideList}>CONTACT ME</a></li>
->>>>>>> main
       </ul>
     </nav>
   );
